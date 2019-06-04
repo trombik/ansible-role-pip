dfccc96ad0c08b0ac0174d0659d68589ae316977
Tomoyuki Sakurai <y@trombik.org>
2019-06-04 17:18:14 +0900

    feature: initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,58 +1,62 @@
 require "spec_helper"
 require "serverspec"
 
-package = "pip"
-service = "pip"
-config  = "/etc/pip/pip.conf"
-user    = "pip"
-group   = "pip"
-ports   = [PORTS]
-log_dir = "/var/log/pip"
-db_dir  = "/var/lib/pip"
+packages = case os[:family]
+          when "freebsd"
+            ["py27-pip", "py36-pip"]
+          end
+pip_executables = case os[:family]
+                 when "freebsd"
+                   ["/usr/local/bin/pip-2.7", "/usr/local/bin/pip-3.6"]
+                 end
+pip_packages = ["platformio"]
 
-case os[:family]
-when "freebsd"
-  config = "/usr/local/etc/pip.conf"
-  db_dir = "/var/db/pip"
+packages.each do |p|
+  describe package(p) do
+    it { should be_installed }
+  end
 end
 
-describe package(package) do
-  it { should be_installed }
+pip_executables.each do |e|
+  describe command("#{e} --version") do
+    its(:exit_status) { should eq 0 }
+    its(:stderr) { should eq "" }
+    its(:stdout) { should match(/^pip\s+\d+\.\d+\.\d+\s/) }
+  end
 end
 
-describe file(config) do
+pip_packages.each do |p|
+  describe package(p) do
+    let(:disable_sudo) { true }
+    it { should be_installed.by("pip") }
+  end
+end
+
+pip_packages.each do |p|
+  describe package(p) do
+    it { should_not be_installed.by("pip") }
+  end
+end
+
+describe file ".local" do
+  it { should exist }
+  it { should be_directory }
+  it { should be_owned_by "vagrant" }
+end
+
+describe file ".local/bin/pio" do
+  it { should exist }
   it { should be_file }
-  its(:content) { should match Regexp.escape("pip") }
+  it { should be_owned_by "vagrant" }
 end
 
-describe file(log_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
+describe command ".local/bin/pio --version" do
+  let(:disable_sudo) { true }
+  its(:exit_status) { should eq 0 }
+  its(:stderr) { should eq "" }
+  its(:stdout) { should match(/PlatformIO,\s+version\s+\d+\.\d+\.\d+/) }
 end
 
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
-case os[:family]
-when "freebsd"
-  describe file("/etc/rc.conf.d/pip") do
-    it { should be_file }
-  end
-end
-
-describe service(service) do
-  it { should be_running }
-  it { should be_enabled }
-end
-
-ports.each do |p|
-  describe port(p) do
-    it { should be_listening }
-  end
+describe command "pio --version" do
+  its(:exit_status) { should_not eq 0 }
 end
